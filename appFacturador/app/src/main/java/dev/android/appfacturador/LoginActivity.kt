c63010853b183a19d1facae555d5e813bb4f63b3
Keybrish <keychanpa@gmail.com>
2023-06-07 21:05:06 -0500

    Interfaz Iniciar Sesión

---
+++ appFacturador/app/src/main/java/dev/android/appfacturador/LoginActivity.kt
@@ -0,0 +1,15 @@
+package dev.android.appfacturador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_login)
+    }
+    //prueba de git con la rama yadi
+}
