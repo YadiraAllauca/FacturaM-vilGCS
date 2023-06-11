017814a96c34f1fea52986feca00e10586eab0bf
Lenin <lmoreno3918@uta.edu.ec>
2023-06-10 22:58:51 -0500

    Metodo de cerrar sesion con Firebase

--- appFacturador/app/src/main/java/dev/android/appfacturador/PruebaLogin.kt
+++ appFacturador/app/src/main/java/dev/android/appfacturador/PruebaLogin.kt
@@ -1,11 +1,33 @@
 package dev.android.appfacturador
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import dev.android.appfacturador.databinding.ActivityPruebaLoginBinding
 
 class PruebaLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityPruebaLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_prueba_login)
+        binding = ActivityPruebaLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cerrarSesion()
+
+        
     }
+
+    fun cerrarSesion() {
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        binding.txtViewSaludo.text = "Bienvenido $email"
+
+        binding.btnCerrarSesion.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+
 }
