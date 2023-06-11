000237ac28ccc0fe453bb1c3e5b4cd04474574e1
Lenin <lmoreno3918@uta.edu.ec>
2023-06-10 21:08:40 -0500

    Actividad PruebaLogin implementada

---
+++ appFacturador/app/src/main/java/dev/android/appfacturador/PruebaLogin.kt
@@ -0,0 +1,11 @@
+package dev.android.appfacturador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PruebaLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba_login)
+    }
+}
