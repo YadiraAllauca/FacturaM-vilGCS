660eca1718b36cc8f13b2038b959db406a0c804d
ArmijosThais <tarmijos02@gmail.com>
2023-07-11 18:04:54 -0500

    Optimizacion AddItem

--- appFacturador/app/src/main/java/dev/android/appfacturador/AddItemActivity.kt
+++ appFacturador/app/src/main/java/dev/android/appfacturador/AddItemActivity.kt
@@ -15,10 +15,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.zxing.integration.android.IntentIntegrator
@@ -31,8 +28,6 @@ class AddItemActivity : AppCompatActivity() {
     lateinit var binding: ActivityAddItemBinding
     lateinit var email: String
     lateinit var shop: String
-    private val fb = Firebase.database
-    private val dr = fb.getReference("Product")
     private val adapter: ProductItemBillAdapter by lazy {
         ProductItemBillAdapter()
     }
@@ -43,6 +38,9 @@ class AddItemActivity : AppCompatActivity() {
     lateinit var barcode: String
     private val REQUEST_CODE_SPEECH_TO_TEXT1 = 1
 
+    private val database: FirebaseDatabase = Firebase.database
+    private val productRef: DatabaseReference = database.getReference("Product")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddItemBinding.inflate(layoutInflater)
@@ -50,11 +48,9 @@ class AddItemActivity : AppCompatActivity() {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
-        //usuario y negocio actual
         val sharedPreferences = getSharedPreferences("PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
         email = sharedPreferences.getString("email", "").toString()
 
-        //recycle view
         recyclerView = binding.rvProducts
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
@@ -68,9 +64,9 @@ class AddItemActivity : AppCompatActivity() {
                 val searchTerm = s.toString().trim()
                 updateProductList(searchTerm)
             }
-
             override fun afterTextChanged(s: Editable?) {}
         })
+
         binding.btnScanner.setOnClickListener {
             initScanner()
         }
@@ -92,19 +88,20 @@ class AddItemActivity : AppCompatActivity() {
             binding.btnAllProducts.background = getDrawable(R.drawable.degradado2)
             binding.btnAllProducts.setTextColor(Color.parseColor("#686868"))
 
-            var pro = ProductHolder.productList.map { it.product }.toMutableList()
-            var filteredProducts = list.filter { pro.contains(it) }.toMutableList()
+            val selectedProducts = ProductHolder.productList.mapNotNull { it.product }
+            val filteredProducts = list.filter { selectedProducts.contains(it) }
             adapter.updateListProducts(filteredProducts)
         }
 
         binding.btnAddItems.setOnClickListener {
-            val intent = Intent(this, AddBillActivity::class.java).apply {}
+            val intent = Intent(this, AddBillActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnMicSearch.setOnClickListener {
             SpeechToTextUtil.startSpeechToText(this@AddItemActivity, REQUEST_CODE_SPEECH_TO_TEXT1)
         }
+
         search()
     }
 
@@ -112,7 +109,6 @@ class AddItemActivity : AppCompatActivity() {
         val user = FirebaseAuth.getInstance().currentUser
         val email = user?.email
 
-        val database = FirebaseDatabase.getInstance()
         val usuariosRef = database.getReference("Empleado")
 
         usuariosRef.orderByChild("correo_electronico").equalTo(email)
@@ -139,32 +135,29 @@ class AddItemActivity : AppCompatActivity() {
             })
     }
 
-    fun loadData() {
-        var listen = object : ValueEventListener {
+    private fun loadData() {
+        productRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 list.clear()
                 snapshot.children.forEach { child ->
                     val negocio = child.child("negocio").value?.toString()
                     if (negocio == shop) {
-                        val product: PRODUCTO? =
-                            child.key?.let {
-                                PRODUCTO(
-                                    child.key.toString(),
-                                    child.child("nombre").value.toString(),
-                                    child.child("precio").value.toString().toFloat(),
-                                    child.child("max_descuento").value.toString().toInt(),
-                                    child.child("id_categoria_impuesto").value.toString(),
-                                    child.child("codigo_barras").value.toString(),
-                                    child.child("imagen").value.toString(),
-                                    child.child("negocio").value.toString()
-                                )
-                            }
+                        val product: PRODUCTO? = child.key?.let {
+                            PRODUCTO(
+                                child.key.toString(),
+                                child.child("nombre").value.toString(),
+                                child.child("precio").value.toString().toFloat(),
+                                child.child("max_descuento").value.toString().toInt(),
+                                child.child("id_categoria_impuesto").value.toString(),
+                                child.child("codigo_barras").value.toString(),
+                                child.child("imagen").value.toString(),
+                                child.child("negocio").value.toString()
+                            )
+                        }
                         product?.let { list.add(it) }
                     }
                 }
                 adapter.updateListProducts(list)
-                recyclerView.adapter = adapter
-
                 adapter.onCheckedChangeListener = { product, isChecked ->
                     if (isChecked) {
                         val productItem =
@@ -175,7 +168,9 @@ class AddItemActivity : AppCompatActivity() {
                         addedList.remove(product)
                         val position =
                             ProductHolder.productList.indexOfFirst { it.product?.nombre == product.nombre }
-                        ProductHolder.productList.removeAt(position)
+                        if (position != -1) {
+                            ProductHolder.productList.removeAt(position)
+                        }
                     }
                     print(addedList.size)
                 }
@@ -184,16 +179,13 @@ class AddItemActivity : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {
                 Log.e("TAG", "messages:onCancelled: ${error.message}")
             }
-        }
-        dr.addValueEventListener(listen)
+        })
     }
 
-    fun updateProductList(searchTerm: String) {
+    private fun updateProductList(searchTerm: String) {
         val filteredList = list.filter { product ->
-            product.nombre.contains(
-                searchTerm,
-                ignoreCase = true
-            ) || product.codigo_barras.contains(searchTerm, ignoreCase = true)
+            product.nombre.contains(searchTerm, ignoreCase = true)
+                    || product.codigo_barras.contains(searchTerm, ignoreCase = true)
         }
         adapter.updateListProducts(filteredList)
     }
@@ -233,23 +225,16 @@ class AddItemActivity : AppCompatActivity() {
         }
     }
 
-    private fun search() = with(binding) {
-        edtBuscador.addTextChangedListener(object : TextWatcher {
+    private fun search() {
+        binding.edtBuscador.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun onTextChanged(
-                filterText: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
+            override fun onTextChanged(filterText: CharSequence?, start: Int, before: Int, count: Int) {
                 if (filterText?.length!! > 0) {
                     val filterList = list.filter { product ->
-                        val fullName =
-                            "${product.nombre}"
+                        val fullName = "${product.nombre}"
                         fullName.uppercase().startsWith(filterText.toString().uppercase()) ||
-                                product.id.uppercase()
-                                    .startsWith(filterText.toString().uppercase())
+                                product.id.uppercase().startsWith(filterText.toString().uppercase())
                     }
                     adapter.updateListProducts(filterList)
                 } else {
