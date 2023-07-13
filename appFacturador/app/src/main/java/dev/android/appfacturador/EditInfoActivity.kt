1c72d14fbcf3372d6b77adc19691a9b5d4f23631
Keybrish <keychanpa@gmail.com>
2023-07-13 10:54:17 -0500

    Cambios edit info

--- appFacturador/app/src/main/java/dev/android/appfacturador/EditInfoActivity.kt
+++ appFacturador/app/src/main/java/dev/android/appfacturador/EditInfoActivity.kt
@@ -2,10 +2,20 @@ package dev.android.appfacturador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import dev.android.appfacturador.databinding.ActivityEditInfoBinding
 
 class EditInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_info)
+        binding = ActivityEditInfoBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
