abec629dd53ba14d7f5b820e6b86e251ace6e4c3
Keybrish <keychanpa@gmail.com>
2023-07-14 19:45:07 -0500

    Menú dark mode

--- appFacturador/app/src/main/java/dev/android/appfacturador/MenuActivity.kt
+++ appFacturador/app/src/main/java/dev/android/appfacturador/MenuActivity.kt
@@ -3,12 +3,19 @@ package dev.android.appfacturador
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.Gravity
+import android.view.View
 import android.view.Window
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import dev.android.appfacturador.databinding.ActivityMenuBinding
 
@@ -23,6 +30,8 @@ class MenuActivity : AppCompatActivity() {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
+        darkMode()
+
         binding.btnArrow.setOnClickListener {
             finish()
         }
@@ -33,15 +42,19 @@ class MenuActivity : AppCompatActivity() {
         if (option == "profile") {
             binding.btnProfile.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blues))
             binding.btnProfiles.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            darkModeOptions(binding.btnProfile, binding.btnProfiles)
         } else if (option == "bill") {
             binding.btnBill.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blues))
             binding.btnBills.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            darkModeOptions(binding.btnBill, binding.btnBills)
         } else if (option == "client") {
             binding.btnClient.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blues))
             binding.btnClients.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            darkModeOptions(binding.btnClient, binding.btnClients)
         } else if (option == "product") {
             binding.btnProduct.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blues))
             binding.btnProducts.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            darkModeOptions(binding.btnProduct, binding.btnProducts)
         }
 
         binding.btnProducts.setOnClickListener {
@@ -73,11 +86,35 @@ class MenuActivity : AppCompatActivity() {
         getWindow().setLayout(370, 950)
         getWindow().decorView.setBackgroundResource(android.R.color.transparent)
         getWindow().setGravity(Gravity.START)
-        //obtener email de usuario
-//        val sharedPreferences = getSharedPreferences("PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+    }
 
-        //eliminar las siguientes dos lineas de abajo para que no salgan los mensajes del email
-//        val email = sharedPreferences.getString("email", "")
-//        Toast.makeText(this, "Valor del email: $email", Toast.LENGTH_SHORT).show()
+    @SuppressLint("ResourceAsColor", "Range")
+    fun darkMode () {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        // Comprueba el modo actual
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            // El modo actual es dark
+            binding.btnContainer.setCardBackgroundColor(Color.parseColor("#353536"))
+            binding.btnClose.setCardBackgroundColor(Color.parseColor("#353536"))
+            binding.btnArrow.setColorFilter(Color.parseColor("#ffffff"))
+            binding.btnProducts.setColorFilter(Color.parseColor("#121212"))
+            binding.btnProduct.setCardBackgroundColor(Color.parseColor("#5c5d5e"))
+            binding.btnClients.setColorFilter(Color.parseColor("#121212"))
+            binding.btnClient.setCardBackgroundColor(Color.parseColor("#5c5d5e"))
+            binding.btnBills.setColorFilter(Color.parseColor("#121212"))
+            binding.btnBill.setCardBackgroundColor(Color.parseColor("#5c5d5e"))
+            binding.btnProfiles.setColorFilter(Color.parseColor("#121212"))
+            binding.btnProfile.setCardBackgroundColor(Color.parseColor("#5c5d5e"))
+        }
+    }
+
+    fun darkModeOptions(card: CardView, icon: ImageView) {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        // Comprueba el modo actual
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            // El modo actual es dark
+            card.setCardBackgroundColor(Color.parseColor("#121212"))
+            icon.setColorFilter(Color.parseColor("#ffffff"))
+        }
     }
 }
